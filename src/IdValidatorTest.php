36bf49630e213ac94cb91391d78b8ac2f3d7c981
jxlwqq <jxlwqq@gmail.com>
2018-09-06 14:08:26 +0000

    Apply fixes from StyleCI

--- src/IdValidatorTest.php
+++ src/IdValidatorTest.php
@@ -3,22 +3,18 @@
  * Created by PhpStorm.
  * User: jxlwqq
  * Date: 2018/9/6
- * Time: 21:51
+ * Time: 21:51.
  */
 
 namespace Jxlwqq\IdValidator;
-use Jxlwqq\IdValidator\IdValidator;
-
 
 class IdValidatorTest extends \PHPUnit_Framework_TestCase
 {
-
     public function testIsValid()
     {
         $idValidator = new IdValidator();
         $this->assertEquals(true, $idValidator->isValid('440308199901101512'));
         $this->assertEquals(false, $idValidator->isValid('440308199901101513'));
-
     }
 
     public function testFakeId()
@@ -26,19 +22,18 @@ class IdValidatorTest extends \PHPUnit_Framework_TestCase
         $idValidator = new IdValidator();
         $this->assertEquals(true, $idValidator->isValid($idValidator->fakeId()));
         $this->assertEquals(true, $idValidator->isValid($idValidator->fakeId(true)));
-
     }
 
     public function testGetInfo()
     {
         $idValidator = new IdValidator();
         $this->assertEquals([
-            'addressCode' => '440308',
-            'address' => '广东省深圳市盐田区',
+            'addressCode'  => '440308',
+            'address'      => '广东省深圳市盐田区',
             'birthdayCode' => '1999-01-10',
-            'sex' => 1,
-            'length' => 18,
-            'checkBit' => '2'],
+            'sex'          => 1,
+            'length'       => 18,
+            'checkBit'     => '2', ],
             $idValidator->getInfo('440308199901101512'));
         $this->assertEquals(false, $idValidator->isValid('440308199901101513'));
     }
